3895485ade25cb15d7f51e38d70e9883b0bdf4a8
MoonlightDreamer <dase.vodemago@yandex.ru>
2025-05-25 23:23:06 +0300

    migration & pgadmin

---
+++ cmet-backend/Migrations/20250525200705_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cmet_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "video_material",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    link = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_video_material", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "video_material");
+        }
+    }
+}
--- cmet-backend/Video/VideoMaterial.cs
+++ cmet-backend/Video/VideoMaterial.cs
@@ -1,16 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cmet_backend.Video
 {
+    [Table(name: "video_material")]
     public class VideoMaterial
     {
         [Key]
+        [Column(name: "id")]
         public required string Id { get; set; }
+
         [Required]
+        [Column(name: "description")]
         public required string Description { get; set; }
+
         [Required]
+        [Column(name: "created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
         [Required]
+        [Column(name: "link")]
         public required string link { get; set; }
     }
 }
