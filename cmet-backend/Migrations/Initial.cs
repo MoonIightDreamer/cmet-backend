5b897af225d3cb321631a8af6f72f25d5580b3ab
MoonlightDreamer <dase.vodemago@yandex.ru>
2025-05-25 21:13:34 +0300

    Add migration

---
+++ cmet-backend/Migrations/Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cmet_backend.Migrations
+{
+    public class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoMaterials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoMaterials", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "VideoMaterials");
+        }
+    }
+}
--- cmet-backend/Program.cs
+++ cmet-backend/Program.cs
@@ -26,6 +26,12 @@ builder.Services.AddScoped<IVideoRepository, VideoRepository>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
